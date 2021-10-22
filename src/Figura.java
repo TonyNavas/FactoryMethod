d878d2f54396c40c5f19a3023417fb471d0f5a81
TonyNavas <tonynavas777@gmail.com>
2021-10-22 13:07:09 -0600

    Creee la clase figura y creee el metodo para obtener el area de la figura

--- src/Figura.java
+++ src/Figura.java
@@ -1,2 +1,11 @@
-public class Figura {
+import java.awt.*;
+
+public abstract class Figura {
+    int lado = 0;
+    public Figura(int lado) {
+        this.lado = lado;
+    }
+    public abstract double getArea();
+    public abstract void dibujar(Graphics g, int x, int y);
+
 }
