ad3fa9c527f09cfc82a6d218bedd5954baca570c
TonyNavas <tonynavas777@gmail.com>
2021-10-22 14:13:10 -0600

    Creando las clases y metodos de la clase cuadrado

--- src/Cuadrado.java
+++ src/Cuadrado.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Cuadrado {
+import java.awt.*;
+
+public class Cuadrado extends Figura {
+    public Cuadrado(int lado) {
+        super(lado);
+    }
+    public double getArea() {
+        return (double) (this.lado * this.lado);
+    }
+
+    public void dibujar(Graphics g, int x, int y) {
+        g.setColor(Color.BLUE);
+        g.drawRect(x, y, lado, lado);
+    }
 }
