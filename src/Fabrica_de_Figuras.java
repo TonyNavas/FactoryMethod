36600a6f68e215ed90bc196ee9abd19488a13995
TonyNavas <tonynavas777@gmail.com>
2021-10-22 13:50:06 -0600

    Se crean las clases y metodos para generar las figuras

--- src/Fabrica_de_Figuras.java
+++ src/Fabrica_de_Figuras.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Fabrica_de_Figuras {
+public class Fabrica_de_Figuras {
+    public static final int CUADRADO = 0;
+    public static final int CIRCULO = 1;
+    public Figura getFigura(int tipo, int lado) {
+        if (tipo == CUADRADO) {
+            return (new Cuadrado(lado));
+        } else {
+            return (new Circulo(lado));
+        }
+    }
 }
